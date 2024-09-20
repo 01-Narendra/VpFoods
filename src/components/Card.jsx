66f78f53f90ed1b379c142b2602dc82549219f42
Narendra Maurya <amanlearntoenvent17@gmail.com>
2024-09-20 18:33:13 +0530

    changes the card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 
 const ProductCard = ({ image, name, price }) => {
     return (
-        <div className="h-[410px] shakeCard transition-all duration-300 hover:cursor-pointer rounded-3xl shadow-xl bg-white border border-x-gray-100 hover:shadow-2xl">
-            {/* Product Image */}
-            <img className="h-[300px] md:w-[350px] w-[300px] rounded-3xl flipCard shadow-xl m-2 object-cover" src={image} alt={name} />
-
-            {/* Product Details */}
-            <div className="px-6 py-4 md:mt-3">
-                <div className="font-bold text-xl mb-2 text-gray-900 font-khand">{name}</div>
-                <p className="text-gray-700 text-xl font-khand">&#8377; {price}</p>
+        <div className="flex justify-center items-center p-4">
+            <div className="cursor-pointer relative p-4 w-64 h-64 rounded-full water-effect shadow-lg hover:shadow-gray-500 hover:shadow-xl transform hover:scale-105 transition-transform duration-300 overflow-hidden">
+                {/* Image */}
+                <img
+                src={image}
+                alt={name}
+                className="w-full h-full object-cover rounded-full"
+                />
+                
+                {/* Hover Overlay */}
+                <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center text-white text-center">
+                <h2 className="text-xl font-bold">{name}</h2>
+                <p className="text-lg mt-2">${price}</p>
+                </div>
             </div>
-        </div>
+            </div>
     );
 };
 
